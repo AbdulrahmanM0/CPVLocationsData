1bf41d4fd5ae9ccf06d5f317812d929078939c43
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-01-22 11:38:48 +0200

    last update

--- src/pages/Locations/SaudiMap.jsx
+++ src/pages/Locations/SaudiMap.jsx
@@ -1,7 +1,7 @@
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
 import saudi from "./mapCord";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "reactstrap";
 import PieChart from "./Charts/PieChartData";
 
@@ -9,6 +9,7 @@ const Map = (props) => {
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const [stateName, setStateName] = useState(null);
   const chartRef = useRef(null);
+  const [lay,setLay] = useState()
 
   const onLocationClick = (event) => {
     const svgRect = event.target.getBoundingClientRect();
@@ -20,16 +21,21 @@ const Map = (props) => {
     setStateName(event.target.getAttribute("name"));
     setPosition({ top: clickY, left: clickX });
   };
+  console.log(lay)
+
+  useEffect(()=>{
+    window.onresize = () =>  setLay(window.innerWidth)
+  },[])
 
   return (
     <div className="container-fluid ">
       <Row className="justify-content-center">
-        <Col sm={12} md={7} lg={7}>
-          <div className="position-relative">
-            <div onClick={onLocationClick}>
+        <Col sm={12} md={12} lg={7}>
+          <div className="position-relative mb-3">
+            <div onClick={onLocationClick} style={{minWidth: '448px'}}>
             <SVGMap map={saudi} />
             </div>
-            {stateName && (
+            {stateName && lay > 800 &&(
               <div                 
               className="position-absolute"
               style={{
@@ -49,12 +55,16 @@ const Map = (props) => {
             )}
           </div>
         </Col>
-        {stateName && (
-          <Col sm={12} md={5} lg={5}>
+        {stateName ? (
+          <Col sm={12} md={12} lg={5}>
             <h5 className="text-center mb-4 p-2" style={{ backgroundColor: '#e5eef7' }}>{stateName}</h5>
             <PieChart legen={true}  showLabel={true} h={100} w={200} stateName={stateName} props={props.data} />
           </Col>
-        )}
+        ):
+          <Col sm={12} md={5} lg={5}>
+            <h5 className="text-center mb-4 p-2" style={{ backgroundColor: '#e5eef7' }}>Chose Region</h5>
+          </Col>
+        }
       </Row>
     </div>
   );
